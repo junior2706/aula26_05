7b67cd2ad1db6c001779ea87bcecb5e23fb29612
junior2706 <juniorcardoso2703@gmail.com>
2022-05-26 21:44:43 -0400

    commit

--- scripts/validacao.js
+++ scripts/validacao.js
@@ -1,11 +1,14 @@
 let form1 = document.forms['meuForm'];
 // alert(form1["uf"].value);
-form1.onsubmit = function(){
+form1.onsubmit = function(event){
+    event.preventDefault;
     if(form1["nome"].value.length <=2){
         alert("Digite mais que 2 caracteres!!!");
         return false;
     }
     let esportes = document.getElementsByClassName("esp");
+    let valoresEsp = Object.values(esportes)
+    alert(typeof(valoresEsp))
     let achei=false;
     esportes.forEach(function(e){
         if(e.checked){
